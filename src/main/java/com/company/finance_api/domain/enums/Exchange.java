f8203a7f7ff9aa51b9b7899cd0f2c1a245694239
TaridaG <abdulkadirtaridag@gmail.com>
2026-01-16 20:04:26 +0300

    add Enums for supported currencies and exchange platforms (desteklenen para birimleri ve borsa platformları için Enum sınıfları eklendi)

---
+++ src/main/java/com/company/finance_api/domain/enums/Exchange.java
@@ -0,0 +1,7 @@
+package com.company.finance_api.domain.enums;
+
+public enum Exchange {
+    BINANCE,
+    BIST,
+    TCMB
+}
---
+++ src/main/java/com/company/finance_api/domain/enums/InstrumentType.java
@@ -0,0 +1,7 @@
+package com.company.finance_api.domain.enums;
+
+public enum InstrumentType {
+    CRYPTO,
+    STOCK,
+    FX
+}
