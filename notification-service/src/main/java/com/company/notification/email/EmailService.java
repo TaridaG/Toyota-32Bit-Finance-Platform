06efaab12fc8561a350576264713a6f6821bb12b
TaridaG <abdulkadirtaridag@gmail.com>
2026-03-16 00:07:42 +0300

    notification: send email on report completed and failed (notification: rapor tamamlandığında ve hata aldığında e-posta gönder)

---
+++ notification-service/src/main/java/com/company/notification/email/EmailService.java
@@ -0,0 +1,10 @@
+package com.company.notification.email;
+
+public interface EmailService {
+
+    void sendEmail(
+            String to,
+            String subject,
+            String body
+    );
+}
---
+++ notification-service/src/main/java/com/company/notification/handler/LogReportNotificationHandler.java
@@ -0,0 +1,62 @@
+package com.company.notification.handler;
+
+import com.company.notification.email.EmailService;
+import com.company.notification.email.EmailTemplateService;
+import com.company.notification.event.ReportCompletedEvent;
+import com.company.notification.event.ReportFailedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class LogReportNotificationHandler implements ReportNotificationHandler {
+    private final EmailService emailService;
+    private final EmailTemplateService emailTemplateService;
+
+    @Override
+    public void handleCompleted(ReportCompletedEvent event) {
+
+        log.info(
+                "REPORT_COMPLETED reportId={} type={} format={}",
+                event.getReportId(),
+                event.getReportType(),
+                event.getExportFormat()
+        );
+
+        String subject =
+                emailTemplateService.buildCompletedSubject(event);
+
+        String body =
+                emailTemplateService.buildCompletedBody(event);
+
+        emailService.sendEmail(
+                "user@email.com",
+                subject,
+                body
+        );
+    }
+
+    @Override
+    public void handleFailed(ReportFailedEvent event) {
+
+        log.warn(
+                "REPORT_FAILED reportId={} reason={}",
+                event.getReportId(),
+                event.getReason()
+        );
+
+        String subject =
+                emailTemplateService.buildFailedSubject(event);
+
+        String body =
+                emailTemplateService.buildFailedBody(event);
+
+        emailService.sendEmail(
+                "user@email.com",
+                subject,
+                body
+        );
+    }
+}
